cfb0a6ddbdfb2f62aaa2436723474a0bf37d05fd
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2024-08-06 11:21:01 -0600

    Initial Commit

---
+++ RetoSpriteKit/RetoSpriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetoSpriteKitApp.swift
+//  RetoSpriteKit
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RetoSpriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
